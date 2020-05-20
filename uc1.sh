6870e00880fcd08bffa8836b552fcb34f4179c77
badal singh <prithvis811@gmail.com>
2020-05-20 22:46:50 +0530

    third usercase

--- uc1.sh
+++ uc1.sh
@@ -1,11 +1,29 @@
 #!/bin/bash -x
 
-wageperhour=20
-fulldayhour=8
-empStatus=$(($RANDOM%2))
-if [ $empStatus -eq 1 ]
-then
-echo "Employee is present"
-else
-   echo  "Employee is absent"
-fi
+
+Part_Time=1; 
+Full_Time=2; 
+total_Salary=0;
+empperhour=20;
+numberofworkingDays=20;
+
+for (( day=1; day<=$numberofworkingDays; day++ ))
+do
+  empCheck=$((RANDOM%3));
+     case $empCheck in 
+	$Full_Time)
+             emphrs=8
+		;;
+	$Part_Time)
+	     emphrs=4
+		;;
+	*)
+	emphrs=0
+		;;
+
+     esac
+
+      salary=$(($emphrs*$empperhour));
+      totalsalary=$(($totalsalary+$salary))
+done
+echo $totalsalary
